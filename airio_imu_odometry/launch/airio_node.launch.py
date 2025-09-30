aafb877fec8b94dfcb2a5c64a8a66fcdbb3a75f3
jba <sjba10815@gmail.com>
2025-10-01 04:19:54 +0900

    airio-cartographer-interpolation function compelete

--- airio_imu_odometry/launch/airio_node.launch.py
+++ airio_imu_odometry/launch/airio_node.launch.py
@@ -4,11 +4,11 @@ from launch_ros.actions import Node
 def generate_launch_description():
     params = "/root/moving_object_estimator_ws/airio_imu_odometry/config/params.yaml"
 
-    airimu_root = "/root/moving_object_estimator_ws/airio_imu_odometry/external/AirIMU"
+    airimu_root = "/root/moving_object_estimator_ws/airio_imu_odometry/external/AirIMU-for-Damvi"
     airimu_ckpt = "/root/moving_object_estimator_ws/airio_imu_odometry/config/airimu/best_model.ckpt"
     airimu_conf = "/root/moving_object_estimator_ws/airio_imu_odometry/config/airimu/codenet.conf"
 
-    airio_root = "/root/moving_object_estimator_ws/airio_imu_odometry/external/Air-IO"
+    airio_root = "/root/moving_object_estimator_ws/airio_imu_odometry/external/Air-IO-for-Damvi"
     airio_ckpt = "/root/moving_object_estimator_ws/airio_imu_odometry/config/airio/best_model.ckpt"
     airio_conf = "/root/moving_object_estimator_ws/airio_imu_odometry/config/airio/motion_body_rot.conf"
 
